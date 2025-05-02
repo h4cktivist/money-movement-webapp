1387b6768d5118526bb0daa1948b1485127d8002
h4cktivist <popovvadim0605@gmail.com>
2025-05-02 21:03:58 +0500

    deploy: allowed hosts change

--- money_movement_app/settings.py
+++ money_movement_app/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+h&&xil#(44(%i+&&n@lvi36ahrrr48=d_p!xp#fa@e#3z#@qh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
